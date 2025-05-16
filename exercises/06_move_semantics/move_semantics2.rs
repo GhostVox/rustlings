813c536fcd91f9eb6de8941de241550b49b6c7c4
Brent-the-carpenter <beh337@gmail.com>
2025-05-15 23:07:41 -0400

    finished sections 5 and 6 of rustlings today.

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -1,9 +1,9 @@
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+    let mut  vec1 = vec;
 
-    vec.push(88);
+    vec1.push(88);
 
-    vec
+    vec1
 }
 
 fn main() {
@@ -19,8 +19,7 @@ mod tests {
     #[test]
     fn move_semantics2() {
         let vec0 = vec![22, 44, 66];
-
-        let vec1 = fill_vec(vec0);
+        let vec1 = fill_vec(vec0.clone());
 
         assert_eq!(vec0, [22, 44, 66]);
         assert_eq!(vec1, [22, 44, 66, 88]);
